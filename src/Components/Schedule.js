ec42f958e14ba1371290e8a4f2fd598419c16705
movarnell <michaelvarnell@icloud.com>
2023-04-01 23:01:47 -0500

    fixed cancel issue

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -2,7 +2,7 @@
 import { useState } from "react";
 
 // Define Schedule component and pass down props
-export default function Schedule({users, deleteUser, updateUsers}) {
+export default function Schedule({ users, deleteUser, updateUsers }) {
     // Define state hooks to store user input
     const [editingUserId, setEditingUserId] = useState(null);
     const [updatedDate, setUpdatedDate] = useState("");
@@ -41,16 +41,16 @@ export default function Schedule({users, deleteUser, updateUsers}) {
         const date = new Date(dateString);
         // If the date is in the past, return an empty string
         if (date < new Date()) {
-            return ""; 
+            return "";
         }
         // Define the options for date formatting
-        const options = { 
-          month: 'long', 
-          weekday: 'long', 
-          day: 'numeric',
-          hour: 'numeric',
-          minute: 'numeric',
-          hour12: true
+        const options = {
+            month: 'long',
+            weekday: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
         };
         // Return the formatted date string
         return new Date(dateString).toLocaleDateString('en-US', options);
@@ -75,23 +75,24 @@ export default function Schedule({users, deleteUser, updateUsers}) {
             {sortedUsers.filter(user => new Date(user.date) > now).map((user) => (
                 <div key={user.id} >
                     {/* Then we format the date using the parameters we listed above in the options, and update based off the user id. */}
-                   <span className="display-6 fw-bold"> {formatDate(editingUserId === user.id ? updatedDate : user.date)} </ span>< br/>
+                    <span className="display-6 fw-bold"> {formatDate(editingUserId === user.id ? updatedDate : user.date)} </ span>< br />
                     <div className="fw-bold">{user.name}<br />
-                     Bringing:  {user.dogName}</div>
+                        Bringing:  {user.dogName}</div>
                     My dog is <span className="fw-bold text-danger">{user.friendly ? "NOT" : ""} </span>dog friendly.
-                   {/* The following will show the edit inputs only for the user.id selected.  */}
+                    {/* The following will show the edit inputs only for the user.id selected.  */}
                     {editingUserId === user.id && (
                         <div className="shadow border border-1 border-dark rounded-3 p-2">
-                            
                             <label htmlFor="updatedDate">New Date & Time:</label>
                             <br />
                             <input type="datetime-local" id="updatedDate" value={updatedDate} onChange={handleDateChange} />
                             <br />
                             <button type="button" onClick={() => handleSave(user)} className="btn btn-primary m-1">Save</button>
+                            <button type="button" onClick={() => setEditingUserId(null)} className="btn btn-secondary m-1">Cancel</button>
                         </div>
                     )}
+
                     <br />
-                    <button type="button" onClick={() => handleEdit(user.id)} className="btn btn-outline-primary m-1 mt-3">{editingUserId === user.id ? "Cancel" : "Edit Time"}</button>
+                    <button type="button" onClick={() => handleEdit(user.id)} className="btn btn-outline-primary m-1 mt-3">Edit Time</button>
                     <button type="button" className="btn btn-danger m-1 mt-3" onClick={() => deleteUser(user.id)}>Delete</button>
                     <hr />
                 </div>
